cc70915e705f0984f0a31215fe7dbf3d937102ef
jeevand073-arch <jeevand073@gmail.com>
2025-12-26 10:52:02 +0530

    Add files via upload

---
+++ calculation  of nagative,posative,zero.py
@@ -0,0 +1,8 @@
+num = int(input("enter the number"))# aking the user for input
+#to check whether it is posative or nagtive,zero
+if num == 0 :
+    print("zero ")
+elif num > 0 :
+    print("posative")
+else:
+    print("neagitve")
